50159af74a2d32242d49ee4bf5a154a6309768c9
Steven-Min <itcloud02147@gmail.com>
2024-02-03 09:21:05 +0800

    Updates

--- src/App.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import MainLayout from './layouts/MainLayout.svelte';
-  import CounterList from "./components/CounterList.svelte";
-</script>
-
-<MainLayout>
-  <CounterList />
-</MainLayout>
